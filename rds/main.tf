cae691a4bd232fa277abccbeebc4a8d3b8c8fcfd
groundbreaker89 <142358906+groundbreaker89@users.noreply.github.com>
2023-11-25 15:59:25 -0600

    edited late

--- rds/main.tf
+++ rds/main.tf
@@ -1,36 +1,3 @@
-# Pulls information from another workspace
-data "terraform_remote_state" "vpc" {
-  backend = "remote"
-
-  config = {
-    organization = "grounbreaker89"
-    workspaces = {
-      name = "vpc"
-    }
-  }
-}
-
-
-# Creates subnet group with private subnets
-resource "aws_db_subnet_group" "default" {
-  name       = "main"
-  subnet_ids = data.terraform_remote_state.vpc.outputs.private_subnets
-}
-
-
-
-resource "aws_db_instance" "default" {
-  allocated_storage    = 10
-  db_name              = "wordpress"
-  engine               = "mysql"
-  engine_version       = "5.7"
-  instance_class       = "db.t3.micro"
-  username             = "admin"
-  password             = "password"
-  parameter_group_name = "default.mysql5.7"
-  skip_final_snapshot  = true
-  db_subnet_group_name  = aws_db_subnet_group.default.name
-}
 # Puuls information from another workspace
 data "terraform_remote_state" "vpc" {
   backend = "remote"
